fcfae2a4be4fbad6a5e5284c17d9ae65b10e4f8e
karostrzelczyk <147985512+karostrzelczyk@users.noreply.github.com>
2023-10-28 18:35:32 +0200

    Update Query.sql

--- Query/Query.sql
+++ Query/Query.sql
@@ -2,9 +2,9 @@
 SELECT a.NAME_1, a.ID, b.PRIZE, count(b.PLACE) as winner
 FROM ATHLETS a
 LEFT JOIN SEASONWINNER b ON a.ID = b.ID_ATHLETS
-where b.PLACE='first'
-group by b.PRIZE, a.id, a.NAME_1
-order by b.PRIZE, winner desc
+WHERE b.PLACE='first'
+GROUP BY b.PRIZE, a.id, a.NAME_1
+ORDER BY b.PRIZE, winner desc
 
 -- które zawodniczki wygrały wszystkie nagrody (czyli dużą, małą kryształowoą kule oraz kula za sprint)
 SELECT wl.ID, wl.NAME_1
@@ -13,9 +13,9 @@ WHERE EXISTS (
     SELECT *
     FROM winner_sprintcrystalglobes ws
     WHERE wl.ID = ws.ID AND wl.NAME_1 = ws.NAME_1
-) and exists (
-select *
-from winner_sprintcrystalglobes wss
- WHERE wl.ID = wss.ID AND wl.NAME_1 = wss.NAME_1);
+) AND EXISTS (
+SELECT *
+FROM winner_sprintcrystalglobes wss
+WHERE wl.ID = wss.ID AND wl.NAME_1 = wss.NAME_1);
 
 
