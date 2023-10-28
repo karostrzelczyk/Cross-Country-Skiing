8100ae5513297a3d1a9fe385cc439507febd5fd5
karostrzelczyk <147985512+karostrzelczyk@users.noreply.github.com>
2023-10-28 18:37:42 +0200

    Update VIEW.sql

--- View/VIEW.sql
+++ View/VIEW.sql
@@ -1,18 +1,18 @@
-create view  winner_smallcrystalglobes as 
+CREATE VIEW  winner_smallcrystalglobes AS 
 SELECT a.NAME_1, a.ID, b.PRIZE, COUNT(b.PLACE) AS winner
 FROM  dbo.ATHLETS AS a 
 LEFT OUTER JOIN dbo.SEASONWINNER AS b ON a.ID = b.ID_ATHLETS
 WHERE  (b.PLACE = 'first') AND (b.PRIZE LIKE '%small%') AND (b.PRIZE NOT LIKE '%sprint%')
 GROUP BY b.PRIZE, a.ID, a.NAME_1
 
-create view  winner_largecrystalglobes as  
+CREATE VIEW  winner_largecrystalglobes AS  
 SELECT a.NAME_1, a.ID, b.PRIZE, COUNT(b.PLACE) AS winner
 FROM dbo.ATHLETS AS a 
 LEFT OUTER JOIN dbo.SEASONWINNER AS b ON a.ID = b.ID_ATHLETS
 WHERE  (b.PLACE = 'first') AND (b.PRIZE LIKE '%large%')
 GROUP BY b.PRIZE, a.ID, a.NAME_1
-
-create view  winner_sprintcrystalglobes as
+  
+CREATE VIEW   winner_sprintcrystalglobes AS
 SELECT a.NAME_1, a.ID, b.PRIZE, COUNT(b.PLACE) AS winner
 FROM dbo.ATHLETS AS a 
 LEFT OUTER JOIN dbo.SEASONWINNER AS b ON a.ID = b.ID_ATHLETS
