6e6d2e0a4ab0783e4fc2973d029cd86ae0c385b1
karostrzelczyk <147985512+karostrzelczyk@users.noreply.github.com>
2023-10-28 16:25:00 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,21 @@
+-- ranking zawodniczek i ile i jakich zdoby³y nagród 
+SELECT a.NAME_1, a.ID, b.PRIZE, count(b.PLACE) as winner
+FROM ATHLETS a
+LEFT JOIN SEASONWINNER b ON a.ID = b.ID_ATHLETS
+where b.PLACE='first'
+group by b.PRIZE, a.id, a.NAME_1
+order by b.PRIZE, winner desc
+
+-- które zawodniczki wygra³y wszystkie nagrody (czyli du¿¹, ma³¹ kryszta³owo¹ kulê oraz kula za sprint)
+SELECT wl.ID, wl.NAME_1
+FROM winner_largecrystalglobes wl
+WHERE EXISTS (
+    SELECT *
+    FROM winner_sprintcrystalglobes ws
+    WHERE wl.ID = ws.ID AND wl.NAME_1 = ws.NAME_1
+) and exists (
+select *
+from winner_sprintcrystalglobes wss
+ WHERE wl.ID = wss.ID AND wl.NAME_1 = wss.NAME_1);
+
+
