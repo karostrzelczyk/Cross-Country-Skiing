a6008308cec523dcef9d3b04b518c3b9b7896a3e
karostrzelczyk <147985512+karostrzelczyk@users.noreply.github.com>
2023-10-28 18:32:29 +0200

    Update Winner_of_the_OG_WC.sql

--- Query/Winner_of_the_OG_WC.sql
+++ Query/Winner_of_the_OG_WC.sql
@@ -1,3 +1,5 @@
+-- Zawodnik, który zdobył złoty medal w Igrzyskach Olimpijskich oraz zdobył złoty medal w Mistrzostwach Świata
+
 SELECT DISTINCT b.ID_ATHLETS, b.NAME_1, c.COUNTRY, c.FIS_CODE
 FROM dbo.COMPETITION AS a 
   INNER JOIN dbo.RESULTS AS b ON a.ID = b.ID_COMPETITION
